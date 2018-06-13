f87e7970446bae6084455bfd4b48e656372af6ed
Gurubalan Harikrishnan <gurubalan.work@gmail.com>
2018-06-13 23:33:50 +0530

    Initial commits.

---
+++ src/com/guru/servletwebapp/Bean.java
@@ -0,0 +1,58 @@
+package com.guru.servletwebapp;
+
+public class Bean {
+    private String fn;
+    private String ln;
+    private String un;
+    private String pass;
+    private String address;
+    private String gender;
+
+    public String getFn() {
+        return fn;
+    }
+
+    public void setFn(String fn) {
+        this.fn = fn;
+    }
+
+    public String getLn() {
+        return ln;
+    }
+
+    public void setLn(String ln) {
+        this.ln = ln;
+    }
+
+    public String getUn() {
+        return un;
+    }
+
+    public void setUn(String un) {
+        this.un = un;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
